f6f01c703c8154cf91179b92c307da73a2a841b1
Jonathan M. Lane <jmlane@gmail.com>
2015-05-23 17:34:14 -0300

    Added 'serve' & 'watch-serve' (via nodemon) to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,46 @@ var gutil = require('gulp-util');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
+var spawn = require('child_process').spawn;
+var packageJson = require('./package.json');
+var nodemon = require('nodemon');
+
+var logBuffer = function (buf) {
+  gutil.log(buf.toString());
+};
+
+gulp.task('dev', ['watch-bundle', 'watch-serve'], function () {
+  gutil.log('Now running in development mode.');
+});
+
+gulp.task('serve', function () {
+  var args = packageJson.scripts.start.split(' ');
+  var command = args.shift();
+  var server = spawn(command, args);
+
+  server.stdout.on('data', logBuffer);
+  server.stderr.on('data', logBuffer);
+
+  server.on('close', function (code) {
+    gutil.log('child process exited with code ' + code);
+  });
+});
+
+gulp.task('watch-serve', function () {
+  var server = nodemon({
+    debug: true,
+    watch: 'server/',
+    stdout: false
+  });
+  server.on('readable', function () {
+    this.stdout.on('data', logBuffer);
+    this.stderr.on('data', logBuffer);
+  });
+
+  server.on('close', function (code) {
+    gutil.log('child process exited with code ' + code);
+  });
+});
 
 gulp.task('bundle', function () {
   var b = browserify();
