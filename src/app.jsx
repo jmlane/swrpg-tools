a815db0ce075afb7296ba812af76a59f034366a9
Jonathan M. Lane <jmlane@gmail.com>
2015-05-29 11:25:09 -0300

    More static character data for component development

--- src/app.jsx
+++ src/app.jsx
@@ -12,6 +12,21 @@ var data = {
       name: "B1-L1",
       player: "Bill",
       career: "Bounty Hunter"
+    },
+    {
+      name: "Onin",
+      player: "Kyle",
+      career: "Hired Gun"
+    },
+    {
+      name: "Tuck Cosmicfall",
+      player: "Heather",
+      career: "Bounty Hunter"
+    },
+    {
+      name: "Prof. Bidbits",
+      player: "Kris",
+      career: "Colonist"
     }
   ]
 };
