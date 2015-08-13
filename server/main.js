7874631549a10dbc22640e01dcd7c7846a11e75c
Jonathan M. Lane <jonathan@thinkingbig.net>
2015-08-13 02:24:57 -0300

    Using cloud-env for Express configuration via environment variables

--- server/main.js
+++ server/main.js
@@ -1,20 +1,11 @@
 var express = require('express');
+var config = require('cloud-env');
 var app = express();
-
-const IS_PROD = process.env.NODE_ENV == 'production';
-
-var config = IS_PROD ? {
-  host: process.env.OPENSHIFT_NODEJS_IP,
-  port: parseInt(process.env.OPENSHIFT_NODEJS_PORT)
-} : {
-  host: 'localhost',
-  port: 8080
-};
 
 app.use('/dist/', express.static('dist'));
 app.use('/', express.static('public'));
 
-var server = app.listen(config.port, config.host, function () {
+var server = app.listen(config.PORT, config.IP, function () {
 
   var host = server.address().address;
   var port = server.address().port;
