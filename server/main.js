3faa7687fe973f72512f5ef4662f4e919365ec1b
Jonathan M. Lane <jonathan@thinkingbig.net>
2015-08-14 17:21:50 -0300

    Initial API stub for characters

--- server/main.js
+++ server/main.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var config = require('cloud-env');
+var api = require('./api/main');
+
 var app = express();
 
-app.use('/dist/', express.static('dist'));
+app.use('/api', api);
+app.use('/dist', express.static('dist'));
 app.use('/', express.static('public'));
 
 var server = app.listen(config.PORT, config.IP, function () {
--- src/app.jsx
+++ src/app.jsx
@@ -2,39 +2,9 @@ var React = require('react');
 var Header = require('./components/Header.jsx');
 var CharacterTable = require('./CharacterTable.jsx');
 
-var data = {
-  characters: [
-    {
-      name: "Shot Dun Yun",
-      player: "Christian",
-      career: "Colonist"
-    },
-    {
-      name: "B1-L1",
-      player: "Bill",
-      career: "Bounty Hunter"
-    },
-    {
-      name: "Onin",
-      player: "Kyle",
-      career: "Hired Gun"
-    },
-    {
-      name: "Tuck Cosmicfall",
-      player: "Heather",
-      career: "Bounty Hunter"
-    },
-    {
-      name: "Prof. Bidbits",
-      player: "Kris",
-      career: "Colonist"
-    }
-  ]
-};
-
 React.render(
   <div className="container" >
     <Header />
-    <CharacterTable data={data.characters} />
+    <CharacterTable />
   </div>,
   document.body);
